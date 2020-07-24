64d22a7cc5de297ed38a2aae5d2499cf10ddca4b
Callum Mackay <mackaycallum99@gmail.com>
2020-07-24 19:28:54 +0100

    Added post-process toggling

---
+++ toggle_post_process.gd
@@ -0,0 +1,19 @@
+extends ViewportContainer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	material = get_material()
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("kb_1"):
+		material.set_shader_param("enabled", !material.get_shader_param("enabled"))
+		#material.set_shader_param("enabled", false)
