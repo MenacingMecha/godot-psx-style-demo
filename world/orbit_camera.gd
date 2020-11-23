fc3c52a86e795c09656c9201ae71d792c0807e6c
Callum Mackay <mackaycallum99@gmail.com>
2020-11-23 17:33:48 +0000

    User can now press "R" to reset scene

--- world/orbit_camera.gd
+++ world/orbit_camera.gd
@@ -3,7 +3,11 @@ extends Spatial
 export (float, 0, 3) var speed = 1
 export (bool) var rotate_camera = true
 
+onready var default_y_rotation : float = rotation.y
+
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		rotate_camera = !rotate_camera
+	if Input.is_action_just_pressed("reset_camera_position"):
+		rotation.y = default_y_rotation
 	rotate_y(1 * speed * delta * float(rotate_camera))
