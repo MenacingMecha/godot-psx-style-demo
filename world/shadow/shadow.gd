cc801eee85d0c8fa05b6b4470b92ec1c4e27fb6b
Callum Mackay <mackaycallum99@gmail.com>
2023-03-07 16:55:51 +0000

    Box shadows are now `MeshInstance` instead of `Sprite3D`

--- world/shadow/shadow.gd
+++ world/shadow/shadow.gd
@@ -1,4 +1,4 @@
-extends Sprite3D
+extends Spatial
 
 export (float, 0, 10) var scale_min = 1
 export (float, 0, 10) var scale_max = 2
@@ -7,7 +7,7 @@ export (float, 0, -2) var position_offset = -0.2
 
 func update_shadow(origin_position: Vector3, distance_from_ground: float) -> void:
 	# set shadow position
-	translation = origin_position + Vector3(0, distance_from_ground - position_offset, 0)
+#	translation = origin_position + Vector3(0, distance_from_ground - position_offset, 0)
 	
 	# set shadow scale
 	var clamped_distance_from_ground = max(distance_from_ground * -1, max_distance_from_ground)
