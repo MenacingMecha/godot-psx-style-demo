85a500ed7f299226a0cd1dd053184d96500ae63c
Callum Mackay <mackaycallum99@gmail.com>
2023-03-09 17:51:00 +0000

    Updated `spatial_sin_pan.gd` to use Godot 4 syntax

--- world/spatial_sin_pan.gd
+++ world/spatial_sin_pan.gd
@@ -1,14 +1,14 @@
-extends Spatial
+extends Node3D
 
 const TRANSLATION_DISTANCE := 1.0
 const TRANSLATION_SPEED := 1.0
 const ROTATION_SPEED := 1.0
 
-export var _reverse_direction := false
+@export var _reverse_direction := false
 
 var _time := 0.0
 
-onready var _default_transform: Transform = get_transform()
+@onready var _default_transform: Transform3D = get_transform()
 
 
 func _process(p_delta: float):
@@ -24,10 +24,10 @@ func restart():
 
 
 static func get_animated_transform(
-	p_default_transform: Transform, p_time: float, p_reverse_direction: bool
-) -> Transform:
+	p_default_transform: Transform3D, p_time: float, p_reverse_direction: bool
+) -> Transform3D:
 	var rotation = Vector3.ONE * p_time * ROTATION_SPEED
 	var translation_direction := -1 if p_reverse_direction else 1
 	var y_pos := sin(p_time * TRANSLATION_SPEED) * TRANSLATION_DISTANCE * translation_direction
-	var offset_transform := Transform(Basis(rotation), Vector3.UP * y_pos)
+	var offset_transform := Transform3D(Basis.from_euler(rotation), Vector3.UP * y_pos)
 	return p_default_transform * offset_transform
